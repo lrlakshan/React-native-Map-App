5f111b78deda4d69cdbd3be9f1f61c5b761c3317
Lakshan Rathnayaka <lakshan@omobio.net>
2019-12-15 15:28:33 +0530

    app::route draw from current location to user selected position

--- mobile_app/App.js
+++ mobile_app/App.js
@@ -5,6 +5,8 @@ import {
   Text,
   TouchableOpacity,
   Platform,
+  Dimensions,
+  Image,
   PermissionsAndroid
 } from "react-native";
 import MapView, {
@@ -13,91 +15,191 @@ import MapView, {
   Polyline,
   PROVIDER_GOOGLE
 } from "react-native-maps";
-import MapviewDirections from "react-native-maps"
+import haversine from "haversine";
+import Geolocation from "react-native-geolocation-service"
+import Destination from "./Src/Components/Destination"
 
-const mode = 'driving'; // 'walking';
-const origin = '6.797698,79.901509';
-const destination = '6.792833,79.900500';
-const APIKEY = 'AIzaSyBGnTT2uj_Jj_p91WdJi__OiuRreC98hac';
-const url = `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&key=${APIKEY}&mode=${mode}`;
+import destinationMarker from "./images/markers/mapMarker.png"
 
-export function decode(t, e) {
-  for (var n, o, u = 0, l = 0, r = 0, d = [], h = 0, i = 0,
-    a = null, c = Math.pow(10, e || 5);
-    u < t.length
-    ;
-  ) {
+const { width, height } = Dimensions.get('window');
 
-    a = null, h = 0, i = 0
-
-    do
-      a = t.charCodeAt(u++) - 63, i |= (31 & a) << h, h += 5;
-    while (a >= 32)
-
-    n = 1 & i ? ~(i >> 1) : i >> 1, h = i = 0
-
-    do
-      a = t.charCodeAt(u++) - 63, i |= (31 & a) << h, h += 5;
-    while (a >= 32)
-
-    o = 1 & i ? ~(i >> 1) : i >> 1, l += n, r += o,
-      d.push([l / c, r / c])
-  }
-  return d.map(
-    function (t) {
-      return { latitude: t[0], longitude: t[1] }
-    })
-}
-
-const initialRegion = {
-  latitude: 6.849836,
-  longitude: 79.952973,
-  latitudeDelta: 0.009,
-  longitudeDelta: 0.009
-}
-
+const ASPECT_RATIO = width / height;
+const LATITUDE = 37.78825;
+const LONGITUDE = -122.4324;
+const LATITUDE_DELTA = 0.0022;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+var id = 0;LATITUDE_DELTA * ASPECT_RATIO
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      coords: []
+      latitude: LATITUDE,
+      longitude: LONGITUDE,
+      latitudeDelta: LATITUDE_DELTA,
+      longitudeDelta: LONGITUDE_DELTA,
+      routeCoordinates: [],
+      distanceTravelled: 0,
+      prevLatLng: {},
+      coordinate: new AnimatedRegion({
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA
+      }),
+      destinationMarker: {
+        coordinate: {
+          latitude: 6.8482321,
+          longitude: 79.953570,
+        },
+        key: id
+      } 
     };
+    this.handlePress = this.handlePress.bind(this);
   }
 
-  componentDidMount(){
-    this.getRout();
+  handlePress(e) {
+    this.setState({
+      destinationMarker:
+      {
+        coordinate: e.nativeEvent.coordinate,
+        key: ++id
+      },
+    });
+    console.log("desLat", this.state.destinationMarker.coordinate.latitude)
+    console.log("desLon", this.state.destinationMarker.coordinate.longitude)
   }
 
-  getRout = () => {
-    fetch(url)
-      .then(response => response.json())
-      .then(responseJson => {
-        if (responseJson.routes.length) {
-          console.log('haaaaaaaaaa', responseJson);
-          this.setState({
-            coords: decode(responseJson.routes[0].overview_polyline.points) // definition below
-          });
-        }else{
-          console.log('baaaaaaaaaa', responseJson.routes);
+  componentDidMount() {
+    const { coordinate } = this.state;
+
+    this.watchID = Geolocation.watchPosition(
+      position => {
+        const { routeCoordinates, distanceTravelled } = this.state;
+        const { latitude, longitude } = position.coords;
+
+        const newCoordinate = {
+          latitude,
+          longitude
+        };
+
+        if (Platform.OS === "android") {
+          if (this.marker) {
+            this.marker._component.animateMarkerToCoordinate(
+              newCoordinate,
+              100
+            );
+          }
+        } else {
+          coordinate.timing(newCoordinate).start();
         }
-      }).catch(e => { console.warn(e) });
+
+        this.setState({
+          latitude,
+          longitude,
+          routeCoordinates: routeCoordinates.concat([newCoordinate]),
+          distanceTravelled:
+            distanceTravelled + this.calcDistance(newCoordinate),
+          prevLatLng: newCoordinate
+        });
+        console.log("oriLat", latitude)
+        console.log("oriLon", longitude)
+      },
+      error => console.log(error),
+      {
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 1000,
+        distanceFilter: 10
+      }
+    );
+    
   }
 
+  componentWillUnmount() {
+    Geolocation.clearWatch(this.watchID);
+  }
+
+  getMapRegion = () => ({
+    latitude: this.state.latitude,
+    longitude: this.state.longitude,
+    latitudeDelta: this.state.latitudeDelta,
+    longitudeDelta: this.state.latitudeDelta
+  });
+
+  calcDistance = newLatLng => {
+    const { prevLatLng } = this.state;
+    return haversine(prevLatLng, newLatLng) || 0;
+  };
 
   render() {
     return (
-      <MapView initialRegion={initialRegion} style={styles.container}>
-        <MapView.Polyline
-          coordinates={[
-            // { latitude: 6.849836, longitude: 79.952973 }, // optional
-            ...this.state.coords,
-            // { latitude: 6.845785, longitude: 79.952611 }, // optional
-          ]}
-          strokeWidth={4}
-        />
-      </MapView>
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          provider={PROVIDER_GOOGLE}
+          showUserLocation
+          followUserLocation
+          loadingEnabled
+          region={this.getMapRegion()}
+          onPress = {this.handlePress}
+          fitToElements ={true}
+          // onRegionChange={region => {
+          //   clearTimeout(this.timerForMap)
+          //   this.timerForMap = setTimeout(() => {
+          //     this.showMarkers(region)
+          //   }, 100)
+          // }}
+          
+        >
+          <Polyline coordinates={this.state.routeCoordinates} strokeWidth={5} />
+          <Marker.Animated
+            ref={marker => {
+              this.marker = marker;
+            }}
+            coordinate={this.state.coordinate}
+          />
+          {(id>0) 
+            ? [<Marker.Animated
+              key={this.state.destinationMarker.key}
+              coordinate={this.state.destinationMarker.coordinate}
+            >
+              {/* <View style={styles.marker}>
+              <Text style={styles.text}>
+                {JSON.stringify(this.state.markers.coordinate)}</Text>
+            </View> */}
+              <Image source={require('./images/markers/mapMarker.png')} style={{ height: 35, width: 35 }} />
+            </Marker.Animated>,
+            <Destination
+              key={this.state.destinationMarker.key-1}
+              originLatitude={this.state.latitude}
+              originLongitude={this.state.longitude}
+              destinationLatitude={this.state.destinationMarker.coordinate.latitude}
+              destinationLongitude={this.state.destinationMarker.coordinate.longitude}
+            />]
+            : null}
+        </MapView>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={[styles.bubble, styles.button]}>
+            <Text style={styles.bottomBarContent}>
+              {JSON.stringify(this.state.coordinate)}
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={[styles.bubble, styles.button]}>
+            <Text style={styles.bottomBarContent}>
+              {JSON.stringify(this.state.routeCoordinates)}</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={[styles.bubble, styles.button]}>
+            <Text style={styles.bottomBarContent}>
+              {JSON.stringify(this.state.destinationMarker.coordinate)}</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     );
   }
 }
@@ -108,6 +210,36 @@ const styles = StyleSheet.create({
     justifyContent: "flex-end",
     alignItems: "center"
   },
+  map: {
+    ...StyleSheet.absoluteFillObject
+  },
+  marker: {
+    backgroundColor: "#550bbc",
+    padding: 5,
+    borderRadius: 5
+  },
+  bubble: {
+    flex: 1,
+    backgroundColor: "rgba(255,255,255,0.7)",
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    borderRadius: 20
+  },
+  latlng: {
+    width: 200,
+    alignItems: "stretch"
+  },
+  button: {
+    width: 80,
+    paddingHorizontal: 12,
+    alignItems: "center",
+    marginHorizontal: 10
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    marginVertical: 1,
+    backgroundColor: "transparent"
+  }
 });
 
 export default App;
---
+++ mobile_app/Src/Components/Destination.js
@@ -0,0 +1,117 @@
+import React from "react";
+import {
+    StyleSheet,
+    View,
+    Text,
+    TouchableOpacity,
+    Platform,
+    PermissionsAndroid
+} from "react-native";
+import MapView, {
+    Marker,
+    AnimatedRegion,
+    Polyline,
+    PROVIDER_GOOGLE
+} from "react-native-maps";
+import MapviewDirections from "react-native-maps"
+
+const mode = 'driving'; // 'walking';
+const origin = '6.8483641,79.9537685';
+const destination = '6.8478286932489585,79.95384380221367';
+const APIKEY = 'AIzaSyBGnTT2uj_Jj_p91WdJi__OiuRreC98hac';
+
+
+export function decode(t, e) {
+    for (var n, o, u = 0, l = 0, r = 0, d = [], h = 0, i = 0,
+        a = null, c = Math.pow(10, e || 5);
+        u < t.length
+        ;
+    ) {
+
+        a = null, h = 0, i = 0
+
+        do
+            a = t.charCodeAt(u++) - 63, i |= (31 & a) << h, h += 5;
+        while (a >= 32)
+
+        n = 1 & i ? ~(i >> 1) : i >> 1, h = i = 0
+
+        do
+            a = t.charCodeAt(u++) - 63, i |= (31 & a) << h, h += 5;
+        while (a >= 32)
+
+        o = 1 & i ? ~(i >> 1) : i >> 1, l += n, r += o,
+            d.push([l / c, r / c])
+    }
+    return d.map(
+        function (t) {
+            return { latitude: t[0], longitude: t[1] }
+        })
+}
+
+const initialRegion = {
+    latitude: 6.849836,
+    longitude: 79.952973,
+    latitudeDelta: 0.009,
+    longitudeDelta: 0.009
+}
+
+
+class Destination extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            coords: []
+        };
+    }
+
+    componentDidMount() {
+        this.getRout();
+    }
+
+    getRout = () => {
+
+        const { originLatitude, originLongitude, destinationLatitude, destinationLongitude } = this.props;
+        const url = `https://maps.googleapis.com/maps/api/directions/json?origin=${originLatitude+","+originLongitude}&destination=${destinationLatitude+","+destinationLongitude}&key=${APIKEY}&mode=${mode}`;
+
+        fetch(url)
+            .then(response => response.json())
+            .then(responseJson => {
+                if (responseJson.routes.length) {
+                    // console.log('haaaaaaaaaa', responseJson);
+                    console.log('origin', originLatitude);
+                    console.log('origin', originLongitude);
+                    this.setState({
+                        coords: decode(responseJson.routes[0].overview_polyline.points) // definition below
+                    });
+                } else {
+                    console.log('baaaaaaaaaa', responseJson.routes);
+                }
+            }).catch(e => { console.warn(e) });
+    }
+
+
+    render() {
+        return (
+                <MapView.Polyline
+                    coordinates={[
+                        // { latitude: 6.849836, longitude: 79.952973 }, // optional
+                        ...this.state.coords,
+                        // { latitude: 6.845785, longitude: 79.952611 }, // optional
+                    ]}
+                    strokeWidth={4}
+                />
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        ...StyleSheet.absoluteFillObject,
+        justifyContent: "flex-end",
+        alignItems: "center"
+    },
+});
+
+export default Destination;
